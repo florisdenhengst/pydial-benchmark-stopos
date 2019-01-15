0c2c70f327bc6ef41cfa849852ffe6300cc4f27e
Floris Hengst <fdhengst@test3.irc.sara.nl.irc.sara.nl>
2019-01-15 10:45:05 +0100

    Store stdout in job-specific dir

--- task.sh
+++ task.sh
@@ -7,8 +7,11 @@ PYDIAL_BASE="${TMPDIR}/pydial/"
 CONF_DIR="${PYDIAL_BASE}config/pydial_benchmarks/partials/dest/"
 PYDIAL_COMMAND="time python ${PYDIAL_BASE}pydial.py train"
 STOPOS_BASE="${HOME}/projects/pydial-stopos/"
-STDOUT_DIR="${HOME}/projects/pydial-stopos/results/stdout/"
-STDERR_DIR="${HOME}/projects/pydial-stopos/results/stderr/"
+STDOUT_DIR="${HOME}/projects/pydial-stopos/results/stdout/${SLURM_ARRAY_JOB_ID}"
+STDERR_DIR="${HOME}/projects/pydial-stopos/results/stderr/${SLURM_ARRAY_JOB_ID}"
+
+mkdir -p "$STDOUT_DIR"
+mkdir -p "$STDERR_DIR"
 
 
 STOPOS_RC="OK"
@@ -21,12 +24,12 @@ if [ "$STOPOS_RC" == "OK" ]; then
 	# the last item of the STOPOS_VALUE variable array contains the random seed
 	stdout_target="${log_base}-${a[-1]}-stdout"
 	stderr_target="${log_base}-${a[-1]}-stderr"
+	rm ${TMPDIR}/$stdout_target
+	rm ${TMPDIR}/$stderr_target
 	command="pushd $PYDIAL_BASE && $PYDIAL_COMMAND ${CONF_DIR}${log_base}.cfg --seed=${a[-1]} > ${TMPDIR}/$stdout_target 2> ${TMPDIR}/$stderr_target && popd"
 	echo "$command"
 	$(eval $command)
 	cp ${TMPDIR}/${stdout_target} ${STDOUT_DIR}
 	cp ${TMPDIR}/${stderr_target} ${STDERR_DIR}
-	cp -r ${PYDIAL_BASE}_benchmarklogs/* ${HOME}/projects/pydial/_benchmarklogs/
-	cp -r ${PYDIAL_BASE}_benchmarkpolicies/* ${HOME}/projects/pydial/_benchmarkpolicies/
 	stopos remove -p pydial-run
 fi
